08f83be002648db981cca975b2f2d18fffeb3428
Waqas Zafar <waqas.zafar@arkhitech.com>
2015-09-14 18:56:19 +0500

    attribute accessibility inplace now

--- app/models/payment_transaction_fee.rb
+++ app/models/payment_transaction_fee.rb
@@ -1,5 +1,6 @@
 class PaymentTransactionFee < ActiveRecord::Base
   unloadable
+  attr_accessible :fee_amount, :fee_percentage, :description
   belongs_to :invoice_payment
   validates :fee_percentage , presence: true
   validates :fee_amount , presence: true
