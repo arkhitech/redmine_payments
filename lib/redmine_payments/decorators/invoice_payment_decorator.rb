39ce937572a3b5b7e313fb90262f5cf932d4188c
Hisham Malik <hisham@arkhitech.com>
2015-09-28 13:56:24 +0500

    typo fixed

--- lib/redmine_payments/decorators/invoice_payment_decorator.rb
+++ lib/redmine_payments/decorators/invoice_payment_decorator.rb
@@ -32,10 +32,10 @@ module RedminePayments
             
             tax_fee_percentage_amount = 0.00
             fixed_tax_amount_over_percentage_fee = 0.00
-            if !self.payment_transaction_fee.fee_tax_percentage.nil?
+            if self.payment_transaction_fee.fee_tax_percentage
               tax_fee_percentage_amount = ((fee_percentage_amount * BigDecimal('%.2f'%self.payment_transaction_fee.fee_tax_percentage)) / 100) 
             end
-            if !self.payment_transaction_fee.fee_tax_percentage.nil?
+            if self.payment_transaction_fee.fee_tax_amount
               fixed_tax_amount_over_percentage_fee = BigDecimal(self.payment_transaction_fee.fee_tax_amount)
             end
             total_tax_amount_over_percentage_fee_amount = tax_fee_percentage_amount + fixed_tax_amount_over_percentage_fee
