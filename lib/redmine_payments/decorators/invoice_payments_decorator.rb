f7c42fb91302cde76c943b9babb6b94fb430e2eb
Hisham Malik <hisham@arkhitech.com>
2015-10-15 02:17:44 +0500

    Update invoice_payments_decorator.rb

--- lib/redmine_payments/decorators/invoice_payments_decorator.rb
+++ lib/redmine_payments/decorators/invoice_payments_decorator.rb
@@ -54,9 +54,6 @@ module RedminePayments
             #              !(User.current.admin? || User.current.allowed_to?(:list_and_edit_invoice_payments , ip.project))
             #            end
           end
-          #byebug
-             @net_amount_hash = Hash[ContactsSetting.major_currencies.map{|currency| [currency, 0.00]}]
-             @transaction_fee_hash = Hash[ContactsSetting.major_currencies.map{|currency| [currency, 0.00]}]
           @tasks_grid = initialize_grid(@invoice_payments, 
             :order => 'id',
             :name => 'grid',
