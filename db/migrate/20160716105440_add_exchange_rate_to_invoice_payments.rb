e0a58ac8831f583e209b76f1b810f75e6743f537
Asim Mushtaq <asim.mushtaq@arkhitech.com>
2016-07-16 11:23:36 +0500

    added exchange rate migrations

---
+++ db/migrate/20160716105440_add_exchange_rate_to_invoice_payments.rb
@@ -0,0 +1,7 @@
+class AddExchangeRateToInvoicePayments < ActiveRecord::Migration
+  def change
+    add_column :invoice_payments, :exchange_rate, :decimal, precision: 5, scale: 2
+    add_column :invoice_payments, :converted_amount, :decimal, precision: 10, scale: 2
+    add_column :invoice_payments, :converted_currency, :string, size: 3
+  end
+end
